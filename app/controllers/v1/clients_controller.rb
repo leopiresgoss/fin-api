6e822b28839633498a6e730718a5f4cf2194002f
Leonardo P Goss <goss.leonardo3@gmail.com>
2023-01-05 10:00:44 -0300

    Fix linter issues

--- app/controllers/v1/clients_controller.rb
+++ app/controllers/v1/clients_controller.rb
@@ -1,12 +1,10 @@
 class V1::ClientsController < ApplicationController
   def account_balance
-    begin
-      client = Client.find(params[:client_id])
-    rescue => exception
-      render json: { error: 'Client not found' }, status: :not_found
-    else
-      balance = client.balance
-      render json: { balance: balance, at: DateTime.now }, status: :ok
-    end
+    client = Client.find(params[:client_id])
+  rescue StandardError => e
+    render json: { error: 'Client not found' }, status: :not_found
+  else
+    balance = client.balance
+    render json: { balance:, at: DateTime.now }, status: :ok
   end
 end
